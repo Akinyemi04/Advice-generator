2201c28ae01596431e9888efb9c50d29c43db09b
unknown <gabrielayoku@gmail.com>
2022-05-21 21:30:49 +0100

    setting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import Body from './Component/Body';
 import './App.css';
+import React, {useState} from 'react'
+import pin from './images/pattern-divider-desktop.svg'
+import tin from './images/icon-dice.svg'
+import rim from './images/pattern-divider-mobile.svg'
 
 function App() {
+  const[val,setVal]=useState()
+  const[dan,setDan]=useState()
+  function Mit(){
+  fetch('https://api.adviceslip.com/advice')
+  .then(res=>res.json())
+  .then(data=>{setVal(data.slip.id);
+  setDan(data.slip.advice)})
+  console.log(val)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='val'>
+      <h1>Advice #{val}</h1>
+      <blockquote>"{dan}"</blockquote>
+      <picture>
+        <source  srcSet={rim} media='(max-width:375px)'></source>
+        <img src={pin} alt='' className='view'></img>
+      </picture>
+      <button onClick={Mit} className='trill' ><img src={tin} alt='tac' id='lip' ></img></button>
+        <Body/>
     </div>
   );
 }
